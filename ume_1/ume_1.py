6970cbb0a1d78abaf734fabba2992556a74672b5
nakachu <naka2@hage.uk>
2016-06-07 19:07:23 +0900

    Finally I did it!!!!!!!!!

--- ume_1/ume_1.py
+++ ume_1/ume_1.py
@@ -1,2 +1,17 @@
+# -*- coding: utf-8 -*-
 a = [10, 12, 40, 5, 1, 21, 90, 72, 21]
+b = [1, 2, 3]
 
+# mode = a
+
+def ume1(mode):
+    print(u'合計')
+    print (sum(mode))
+    print(u'平均')
+    print (sum(mode)/len(mode))
+    print('saisho')
+    print(min(mode))
+    print('saidai')
+    print(max(mode))
+
+ume1(b)
